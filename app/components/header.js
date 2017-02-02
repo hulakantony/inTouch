b0d77701ec2bdecd1b7a983860b1087a884ff90b
Lucker2120 <makurin91@gmail.com>
2017-02-02 15:25:59 +0200

    add logo

--- app/components/header.js
+++ app/components/header.js
@@ -6,7 +6,10 @@ export default function Header () {
     return (
     <nav className="navbar navbar-default">
     	<div className="container-fluid">
-            <a href="/" className="logo"><i className="fa fa-shower" aria-hidden="true"></i></a>
+            <a href="/" className="logo">
+                <span className="logo-in">IN</span>
+                <span className="logo-touch">touch</span>
+            </a>
     		<ul className="nav nav-pills header-nav">
                 <li role="presentation"  >
 	  				<Link to={ '/auth' } activeClassName='active' >Sign In</Link>
