5a9c885810827c955793d6e97ce27b1790049f59
Kate <katerynahovor@gmail.com>
2017-02-03 12:20:07 +0200

    Return active users

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -6,13 +6,12 @@ const User = require('../../models/user');
 
 module.exports = function (app) {
   // route for getting users. ?active=true will return only active users 
-  //TODO Retrun actuve users
   //TODO Make private route
-  
+
   app.get('/users', function (req, res) {
     let base = req.query.active;
-    let where = base == {} ? {active: true} : {};
-    User.find({}, function (err, users) {
+    let where = (base == 'true') ? {$where: "this.local.active==true"} : {};
+    User.find(where, function (err, users) {
       if (err) throw err;
       res.json(users)
     })
